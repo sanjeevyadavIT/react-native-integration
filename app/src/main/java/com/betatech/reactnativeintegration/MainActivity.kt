9e0d1feabd1275a11aaa10fe4f47ced0285813e2
Sanjeev Yadav <sanjeevy133@protonmail.com>
2021-10-16 09:43:13 +0530

    Add button

--- app/src/main/java/com/betatech/reactnativeintegration/MainActivity.kt
+++ app/src/main/java/com/betatech/reactnativeintegration/MainActivity.kt
@@ -2,10 +2,23 @@ package com.betatech.reactnativeintegration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.betatech.reactnativeintegration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.reactBtn.setOnClickListener{
+            Toast.makeText(this, "Launch React Native Screen", Toast.LENGTH_SHORT).show()
+        }
     }
 }
